f974ec9a783bd2f1a267e112205c595d4e079741
root <root@localhost.localdomain>
2019-10-25 06:38:37 -0400

    fix stack alignement

--- srcs_s/ft_end.s
+++ srcs_s/ft_end.s
@@ -17,10 +17,10 @@ ft_end:
 	pop rdi
 	mov rsp, rbp
 	pop rbp
-	add rsp, 4
-	cmp DWORD [rsp - 4], 1
+	add rsp, 8
+	cmp DWORD [rsp - 8], 1
 	je jmp1
-	cmp DWORD [rsp - 4], 2
+	cmp DWORD [rsp - 8], 2
 	je jmp2
 jmp1:
 	jmp -1
