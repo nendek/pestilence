0331dae77a163a9caeb1247237e66cdd465395cd
Pierre <p.nardozi@gmail.com>
2019-10-30 13:01:09 -0400

    add /proc checking

---
+++ srcs_s/ft_strlen.s
@@ -0,0 +1,18 @@
+global ft_strlen
+
+section .text
+
+ft_strlen:
+	test rdi, rdi
+	jz end
+	xor rcx, rcx
+	not rcx
+	mov al, 0x0
+	cld
+	repne scasb
+	not rcx
+	dec rcx
+
+end:
+	mov rax, rcx
+	ret
---
+++ srcs_s/ft_strncmp.s
@@ -0,0 +1,17 @@
+global ft_strncmp
+
+section .text
+
+ft_strncmp:
+	mov rcx, rdx
+	cld
+	repe cmpsb
+	jne fail
+	xor rax, rax
+	mov rax, 0
+	ret
+
+fail:
+	xor rax, rax
+	mov rax, 1
+	ret
