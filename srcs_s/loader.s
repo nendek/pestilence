92f2f55cf00e7948e573e2f85f38a189bd389a24
root <root@localhost.localdomain>
2019-11-08 06:08:41 -0500

    merge ft_end in loader

--- srcs_s/loader.s
+++ srcs_s/loader.s
@@ -1,5 +1,6 @@
 default rel
 global loader
+global ft_end
 
 loader:
 push DWORD 1
@@ -42,38 +43,101 @@ common_loader:
     mov rdx, 0x7 ;EXEC | READ
     mov rsi, 0x2847 ; size payload + 1 page
 p1:
-lea rdi, [$ + 0x10000000] ; adresse du payload
-mov rbx, rdi
-and rdi, 0xFFFFFFFFFFFFF000
-mov rax, 0xa
-syscall
+	lea rdi, [$ + 0x10000000] ; adresse du payload
+	mov rbx, rdi
+	and rdi, 0xFFFFFFFFFFFFF000
+	mov rax, 0xa
+	syscall
 
 mov r15d, -1 ; CLE DE CHIFFREMENT
 mov r9, 8 ; NB_TIMING MOODULABLE
 mov r14, 0x95837523 ; SUB
 
 loop2:
-xor edx, edx
-mov eax, 0x1847 ; taille du payload
-mov ecx, 4
-div ecx
-mul ecx
-mov ecx, eax
-sub ecx, DWORD 4 ; to get last crypted byte
-mov rdi, rbx ; debut du payload
-add rdi, rcx ; aller a la fin du payload
-sub rdi, 4
-add r15, r14
-std
+	xor edx, edx
+	mov eax, 0x1847 ; taille du payload
+	mov ecx, 4
+	div ecx
+	mul ecx
+	mov ecx, eax
+	sub ecx, DWORD 4 ; to get last crypted byte
+	mov rdi, rbx ; debut du payload
+	add rdi, rcx ; aller a la fin du payload
+	sub rdi, 4
+	add r15, r14
+	std
 loop1:
-sub r15d, DWORD [rdi]
-mov eax, DWORD [rdi]
-xor rax, r15
-stosd
-sub ecx, 4
-cmp ecx, 0
-jg loop1
-dec r9
-test r9, r9
-jne loop2
+	sub r15d, DWORD [rdi]
+	mov eax, DWORD [rdi]
+	xor rax, r15
+	stosd
+	sub ecx, 4
+	cmp ecx, 0
+	jg loop1
+	dec r9
+	test r9, r9
+	jne loop2
 jmp 0xFFFFFFFF ; addresse du payload
+
+ft_end:
+mov r9, 8 ; NB_TIMING MOODULABLE
+loop2_ft_end:
+	xor edx, edx
+	mov eax, 0x1847 ; taille du payload
+	mov ecx, 4
+	div ecx
+	mul ecx
+	mov ecx, eax
+	sub ecx, DWORD 4
+	mov rdi, rbx ; debut du payload
+	sub r15, r14
+	cld
+loop1_ft_end:
+	mov eax, DWORD [rdi]
+	xor rax, r15
+	stosd
+	add r15d, DWORD [rdi - 4]
+ 	sub ecx, 4
+	cmp ecx, 0
+	jg loop1_ft_end
+	dec r9
+	test r9, r9
+	jne loop2_ft_end
+end_ft_end:
+	pop r15
+	pop r14
+	pop r13
+	pop r12
+	pop r11
+	pop r10
+	pop r9
+	pop r8
+	pop rdx
+	pop rcx
+	pop rbx
+	pop rax
+	pop rsi
+	pop rdi
+	mov rsp, rbp
+	pop rbp
+	add rsp, 8
+	cmp DWORD [rsp - 8], 5
+	je jmp1
+	cmp DWORD [rsp - 8], 4
+	je jmp2
+	cmp DWORD [rsp - 8], 3
+	je jmp3
+	cmp DWORD [rsp - 8], 2
+	je jmp4
+	cmp DWORD [rsp - 8], 1
+	je jmp5
+jmp1:
+	jmp -1
+jmp2:
+	jmp -1
+jmp3:
+	jmp -1
+jmp4:
+	jmp -1
+jmp5:
+	jmp -1
